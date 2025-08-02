0033670e9e996b3aa6f85fc7ecabedcbde94970b
Andreea Negoie <59620929+andreeanegoie@users.noreply.github.com>
2025-08-02 20:45:57 +0100

    Create 9_Hello.cpp
    Fill in the blanks to complete the code so the function runs and outputs "Hello" to the console
    
    #include <iostream>
    using namespace std;
    
    ____ hello(){
      ____ << "Hello" << ___;
    }
    
    int main(){
    
      _______
    
      return 0;
    }

---
+++ 9_Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void hello(){ // Function definition: prints "Hello" to the screen
+  cout << "Hello" << endl;
+}
+
+int main(){
+
+ hello();  // Call the hello function
+
+return 0;
+}
