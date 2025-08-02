a3f19bbcd87d584769cd8fa1f47ad44a24532657
Andreea Negoie <59620929+andreeanegoie@users.noreply.github.com>
2025-08-02 17:16:47 +0100

    Update 1_CodingIsCool.cpp
    Fill in the blanks in the code below so the program outputs "Coding is Cool" to the console.
    
    #include <iostream>
    #include <string>
    using namespace std;
    
    int main() {
         _____ << _____ << endl;
    }

--- 1_CodingIsCool.cpp
+++ 1_CodingIsCool.cpp
@@ -1,7 +1,13 @@
+// Include the input-output stream library for using cout and cin
 #include <iostream>
+// Include the string library to use string variables (not used here, but included)
 #include <string>
+// Use the standard namespace to avoid writing std:: before standard functions
 using namespace std;
-
+// Main function - the entry point of the program
 int main() {
-cout<< "Coding is Cool" << endl;
+    // Output the text "Coding is Cool" followed by a newline
+    cout << "Coding is Cool" << endl;
+    // Return 0 to indicate that the program ended successfully
+    return 0;
 }
