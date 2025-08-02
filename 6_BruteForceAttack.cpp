4c128741011dedb58d0ba3fac48dc0bdb1ca42af
Andreea Negoie <59620929+andreeanegoie@users.noreply.github.com>
2025-08-02 19:19:35 +0100

    Create 6_BruteForceAttack.cpp
    You are an inexperienced hacker trying to find the correct password to enter a safe...
    
    Write a programme that defines the correct password as 12345, and allows users to enter password attempts until they reach the correct answer.
    
    Your programme should use a while loop to allow the user to keep entering password attempts until correct. Once the correct password is entered the program should output an appropriate message e.g "Welcome to the Secure Area"
    
    Optional Requirements
    Modify the solution to Brute-Force Attack to include a maximum of 5 password attempts. Each time the user enters an incorrect password, they should be told how many password attempts remain. If there are 5 failed password attempts the while loop should end and the program should inform the user that the authorities have been alerted!

---
+++ 6_BruteForceAttack.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string right_pass="12345";//Initialise the right password
+string password=""; //To store the user password
+int attempts=5;//Limit the number of attempts to 5
+
+int main() {
+cout<<"Select a password:"<<endl;
+cin>>password;
+while ((password!=right_pass)&&(attempts>>1)){ //Loop while the password is wrong and the user still has attempts left
+attempts--;//Decrease the number of attemts left
+cout<<"You have "<<attempts<<" more attempts. Input another password: ";//Inform the user about the number of attempts left
+cin>>password;}//read another password
+cout<<"Welcome to the Secure Area"<<endl;//Right password confirmation
+
+return 0;
+} 
+    
