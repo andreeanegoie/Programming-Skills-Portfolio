17a49219aebb1a8643c1119f34615afb91dd49af
Andreea Negoie <59620929+andreeanegoie@users.noreply.github.com>
2025-08-04 14:35:17 +0100

    Update 3_Bibliography.cpp
    Create a program that prints your name, hometown and age to the console. The program has the following constraints:
    
    Each item should be stored using a variable of an appropriate data type.
    Each item should be printed on a new line.
    You can only use cout once.
    Advanced requirements
    Have the user input the values for name, hometown and age
    Try giving the program both your first and second name when asked. What do you notice? Can you provide a fix?
    Try entering a string or letter value when asked to enter your age. What happens? Can you provide a fix?
    If completing the optional requirements you may use cout more than once to request user input. However the output of information must be performed with a single cout

--- 3_Bibliography.cpp
+++ 3_Bibliography.cpp
@@ -7,11 +7,12 @@ int age; //Declare age as an integer
 string name="", town=""; // Declare 2 empty strings for name and town
 cout<<"What is your name? "<<endl;
 //cin>>name; can only be used to read one name
-getline(cin,name);
+getline(cin,name);//The fix provided for this issue is replacing the cin with getline (Source:https://www.w3schools.com/cpp/cpp_strings_input.asp)
 cout<<"What is your hometown? "<<endl;
 cin>>town;
 cout<<"What is your age?  "<<endl;
 cin>>age;
+ //(Source: https://stackoverflow.com/questions/10349857/how-to-handle-wrong-data-type-input)
  while (cin.fail()) { // Keep asking for input until a valid integer is entered
         cin.clear();   // Clear the error flag on cin
         cin.ignore(1000, '\n');  // Discard the invalid input
